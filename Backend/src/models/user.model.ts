8354a6c72a2419f90cefabab979b4cf391777abb
patoliya <developer.patoliya@gmail.com>
2025-09-20 12:11:19 +0530

    Feat : Attendence model

--- Backend/src/models/user.model.ts
+++ Backend/src/models/user.model.ts
@@ -1,17 +1,27 @@
-import { Schema, model, InferSchemaType } from 'mongoose';
+import { Schema, model, InferSchemaType } from "mongoose";
 
 const userSchema = new Schema(
   {
     first_name: { type: String, required: true, trim: true },
     last_name: { type: String, required: true, trim: true },
-    email: { type: String, required: true, unique: true, lowercase: true, index: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
     password_hash: { type: String, required: true },
     profile_pic_url: { type: String, required: true },
     attendance_pic_url: { type: String, required: true },
-    registration_mode: { type: String, enum: ['upload', 'live'], required: true },
+    registration_mode: {
+      type: String,
+      enum: ["upload", "live"],
+      required: true,
+    },
   },
-  { timestamps: true }
+  { strict: true, timestamps: true }
 );
 
 export type UserDoc = InferSchemaType<typeof userSchema>;
-export const UserModel = model<UserDoc>('User', userSchema);
+export const UserModel = model<UserDoc>("User", userSchema);
